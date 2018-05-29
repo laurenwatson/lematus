fe3dff2fdd0299b9b49f581eb087ea7049be9ddb
tomsbergmanis <tomsbergmanis@gmail.com>
2018-05-29 20:47:39 +0100

    refactored version

---
+++ data/accuracy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+
+reference = sys.argv[1]
+
+ref = open(reference, "r").read().split("\n")
+i = 0
+j = 0
+for line in sys.stdin:
+    line = line.strip()
+    if ref[i] == line:
+        j += 1
+    i+=1
+print(float(j)/i)
