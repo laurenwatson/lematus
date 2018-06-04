aa41e9188dd50d8143fc1781d0db9b7550e9e07b
Lauren Watson <laurenwatson@Laurens-MacBook-Pro.local>
2018-06-04 23:35:47 +0200

    Add number of wordform=lemma information to statistics

--- data_stats.py
+++ data_stats.py
@@ -12,23 +12,29 @@ model_name = sys.argv[1]
 
 for data_set in ['train', 'dev', 'test']:
 
+
     for lang in languages:
 
         model=lang + "-" + model_name
 
         num_lines = 0
-
+        num_copied = 0.0
         target_filepath = "data/languages/" + model +"/train-targets"
         source_filepath = "data/languages/" + model +"/train-sources"
 
         with open(target_filepath, "r") as t:
             with open(source_filepath, "r") as s:
-                for line in s:
-                    num_lines += 1
-                    line_content = line.split("<lc>")[1].split("<rc>")
-                    wordform = "".join(line_content[0].strip().split()).lower()
-                    lemma = "".join(t.readline().strip().split()[1:-1]).lower()
+                while num_lines<10000:
+                    for line in s:
+                        num_lines += 1
+                        line_content = line.split("<lc>")[1].split("<rc>")
+                        wordform = "".join(line_content[0].strip().split()).lower()
+                        lemma = "".join(t.readline().strip().split()[1:-1]).lower()
+
+                        if wordform == lemma:
+                            num_copied+=1
 
 
 
         print( lang + ' has number of ' + data_set + ' lines: ', num_lines)
+        print( lang + ' has percentage of ' + data_set + ' tokens copied: ', num_copied/num_lines)
