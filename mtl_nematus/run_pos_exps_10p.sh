f001a2d82db51b338832626e683b718c99e0e76e
laurenwatson <watsonla@tcd.ie>
2018-07-16 16:36:26 +0100

    Update training scripts

--- mtl_nematus/run_pos_exps_10p.sh
+++ mtl_nematus/run_pos_exps_10p.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 languages=( English )
-exp_id='pos-10p-1k-run'
+exp_id='pos-10p-10k-run'
 for lang in "${languages[@]}"
 do
     echo 'TRAINING'
     # language run experiment_id size
-    ./train_pos_10p_alternate.sh ${lang} 1 ${exp_id} 1
+    ./train_pos_10p_alternate.sh ${lang} 1 ${exp_id} 10
 
     echo 'VALIDATING'
     # language run experiment_id
--- mtl_nematus/run_pos_exps_10p_2.sh
+++ mtl_nematus/run_pos_exps_10p_2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
 languages=( English )
+exp_id='pos-10p-10k-run'
 for lang in "${languages[@]}"
 do
-    # for data set in data/languages/Latvian-20-char-context parameters are:
-    ./train_pos_10p_alternate.sh ${lang} 2 
-    # where v1 is an experiment identifier (can be any string)
+    echo 'TRAINING'
+    # language run experiment_id size
+    ./train_pos_10p_alternate.sh ${lang} 2 ${exp_id} 10
+
+    echo 'VALIDATING'
+    # language run experiment_id
+    ./validae_pos_aux.sh ${lang} 2  ${exp_id}
+
 done
